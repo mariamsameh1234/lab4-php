36590bd8528a1cb249961da72413583b7e96e287
mariamsameh1234 <engysameh199@gmail.com>
2025-03-03 12:05:46 -0800

    Update business_logic.php

--- business_logic.php
+++ business_logic.php
@@ -1,26 +1,34 @@
-
 <?php
-include_once 'database.php';
+include_once 'datebase.php';
 
 function insert_user($pdo, $name, $email, $password, $room_id, $ext, $profile_picture) {
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    return insert($pdo, "user", ["name", "email", "password", "room_id", "ext", "profile_picture"], [$name, $email, $hashed_password, $room_id, $ext, $profile_picture]);
+    return insert($pdo, "users", ["name", "email", "password", "room_id", "ext", "profile_picture"], [$name, $email, $hashed_password, $room_id, $ext, $profile_picture]);
 }
+
 
 function get_all_rooms($pdo) {
     return selectAll($pdo, "rooms");
 }
 
 function get_all_users($pdo) {
-    return select($pdo, "user LEFT JOIN rooms ON user.room_id = rooms.id", ["user.*", "rooms.name AS room_name"], "", []);
+    return select($pdo, "users LEFT JOIN rooms ON users.room_id = rooms.id", ["users.*", "rooms.name AS room_name"], "", []);
+}
+
+function get_user($pdo, $id) {
+    $columns = ["name", "email", "ext", "room_id", "profile_picture"];
+    $condition = "id = ?";
+    return select($pdo, "users", $columns, $condition, [$id]);  // تعديل من "user" إلى "users"
 }
 
 function update_user($pdo, $id, $name, $email, $room_id, $ext) {
-    return update($pdo, "user", ["name", "email", "room_id", "ext"], [$name, $email, $room_id, $ext, $id], "id = ?");
+    return update($pdo, "users", ["name", "email", "room_id", "ext"], [$name, $email, $room_id, $ext], "id = ?");  // تعديل من "user" إلى "users"
 }
 
 function delete_user($pdo, $id) {
-    return delete($pdo, "user", "id = ?", [$id]);
+    return delete($pdo, "users", "id = ?", [$id]);  // تعديل من "user" إلى "users"
 }
+
 ?>
+
 
