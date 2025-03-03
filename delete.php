29e472e36768f4991d29755022df5a04dc5d768b
mariamsameh1234 <engysameh199@gmail.com>
2025-03-03 12:08:17 -0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,18 @@
 <?php
-session_start();
-include 'database.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL); 
+include_once 'business_logic.php';
 
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    delete_user($pdo, $id);
-}
+    $userId = $_GET['id'];
+    delete_user($pdo, $userId); 
 
-header("Location: display_users.php");
-exit();
+  
+    header("Location: display_users.php");
+    exit;
+}
+?>
+
+
 
